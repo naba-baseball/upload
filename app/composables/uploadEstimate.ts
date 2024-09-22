a16e87ee0023dc5e3456d7a71704af4e64f09b7a
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2024-09-22 15:27:41 -0600

    more accurate upload estimate, wanna try in prod

--- app/composables/uploadEstimate.ts
+++ app/composables/uploadEstimate.ts
@@ -2,16 +2,16 @@
  * @param fileSize in bytes
  * @returns estimated time in seconds formatted as a string
  */
-export function useUploadEstimate(fileSize: MaybeRefOrGetter<number | null | undefined>): Ref<string> {
+export function useUploadEstimate(fileSize: MaybeRefOrGetter<number | null | undefined>) {
   const { downlink } = useNetwork()
-  const _fileSize = computed(() => (toValue(fileSize) || 0) / 1024 / 1024)
-  const mbps = computed(() => ((downlink.value ?? 0) * 25) / 1000)
-  const estimatedTime = computed(() => {
-    if (!_fileSize.value)
+  const fileSizeMb = computed(() => (toValue(fileSize) || 0) / 1024 / 1024)
+  const mbps = computed(() => ((downlink.value ?? 0)))
+  const seconds = computed(() => (fileSizeMb.value * 8) / mbps.value)
+  const message = computed(() => {
+    if (!fileSizeMb.value)
       return ''
       // get file size
-    const result = (_fileSize.value * 0.125) / mbps.value
-    return `${Math.round(result)} seconds`
+    return `${Math.round(seconds.value)} seconds`
   })
-  return estimatedTime
+  return { message, mbps, seconds }
 }
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -18,10 +18,15 @@ async function handleUpload(event: SubmitEvent) {
   const file = new File([formFile], 'reports.tar.gz')
 
   loading.value = true
-  loadingProgress.value = 0
+  const startingProgress = 1 / (Math.ceil((file.size / 1024 / 1024) / 5)) * 100
+  loadingProgress.value = startingProgress
   const { completed, progress } = startUpload(file)
-  const stopProgressSync = watchEffect(() => { loadingProgress.value = progress.value })
+  const stopProgressSync = watchEffect(() => {
+    if (progress.value > startingProgress)
+      loadingProgress.value = progress.value
+  })
   completed.finally(() => { loading.value = false })
+  const counterInterval = setInterval(() => { timer.value += 1 }, 1000)
   await completed.catch((err) => {
     toast.add({
       title: 'Upload failed',
@@ -29,7 +34,7 @@ async function handleUpload(event: SubmitEvent) {
       timeout: 0,
     })
     throw err
-  }).finally(() => { stopProgressSync() })
+  }).finally(() => { stopProgressSync(); clearInterval(counterInterval) })
   loadingProgress.value = 100
   await $fetch('/api/deploy', { method: 'POST' }).catch((err) => {
     toast.add({
@@ -48,51 +53,51 @@ async function handleUpload(event: SubmitEvent) {
 
 /** in megabytes */
 const fileSize = ref<number>()
-const estimatedTime = useUploadEstimate(fileSize)
+const { message: estimatedTime, mbps } = useUploadEstimate(fileSize)
 const { isSupported: isNetworkSupported } = useNetwork()
+
+const timer = ref(0)
 </script>
 
 <template>
-  <section>
-    <UCard class="relative overflow-hidden">
-      <template #header>
-        <div class="text-lg font-bold">
-          Upload the reports file here
-        </div>
-      </template>
-      <form
-        name="upload"
-        method="POST"
-        enctype="multipart/form-data"
-        class="space-y-4"
-        @submit.prevent="handleUpload($event as SubmitEvent)"
+  <UCard as="section">
+    <template #header>
+      <div class="text-lg font-bold">
+        Upload the reports file here
+      </div>
+    </template>
+    <form
+      name="upload"
+      method="POST"
+      enctype="multipart/form-data"
+      class="space-y-4"
+      @submit.prevent="handleUpload($event as SubmitEvent)"
+    >
+      <UFormGroup label="Upload reports file">
+        <UInput type="file" name="file" icon="i-lucide-file-archive" required @change="fileSize = ($event?.[0]?.size) ?? undefined" />
+        <template #help>
+          <ClientOnly>
+            <template #fallback>
+              <span>&nbsp;</span>
+            </template>
+            <span v-if="isNetworkSupported">
+              Based off your estimated network speed of {{ mbps }} Mbps, your upload should take {{ estimatedTime }} <br>
+              time: {{ timer }}
+            </span>
+            <span v-else>
+              Upload could take several minutes
+            </span>
+          </ClientOnly>
+        </template>
+      </UFormGroup>
+      <UButton
+        icon="i-lucide-upload"
+        type="submit"
+        :loading="loading"
       >
-        <UFormGroup label="Upload reports file">
-          <UInput type="file" name="file" icon="i-lucide-file-archive" required @change="fileSize = ($event?.[0]?.size) ?? undefined" />
-          <template #help>
-            <ClientOnly>
-              <template #fallback>
-                <span>&nbsp;</span>
-              </template>
-              <span v-if="isNetworkSupported">
-                Your estimated upload time:
-                <span>{{ estimatedTime }}</span>
-              </span>
-              <span v-else>
-                Upload could take several minutes
-              </span>
-            </ClientOnly>
-          </template>
-        </UFormGroup>
-        <UButton
-          icon="i-lucide-upload"
-          type="submit"
-          :loading="loading"
-        >
-          Upload file
-        </UButton>
-        <UProgress indicator :color="loadingProgress === 100 ? 'green' : 'primary'" :value="loadingProgress" />
-      </form>
-    </UCard>
-  </section>
+        Upload file
+      </UButton>
+      <UProgress indicator :color="loadingProgress === 100 ? 'green' : 'primary'" :value="loadingProgress" />
+    </form>
+  </UCard>
 </template>
