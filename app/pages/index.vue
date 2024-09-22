3ebdf3e92914b0ae84dac58c55c46f1609f45e6e
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2024-09-22 16:29:37 -0600

    remove upload estimate. Its just way too inaccurate

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -26,7 +26,6 @@ async function handleUpload(event: SubmitEvent) {
       loadingProgress.value = progress.value
   })
   completed.finally(() => { loading.value = false })
-  const counterInterval = setInterval(() => { timer.value += 1 }, 1000)
   await completed.catch((err) => {
     toast.add({
       title: 'Upload failed',
@@ -34,7 +33,7 @@ async function handleUpload(event: SubmitEvent) {
       timeout: 0,
     })
     throw err
-  }).finally(() => { stopProgressSync(); clearInterval(counterInterval) })
+  }).finally(() => { stopProgressSync() })
   loadingProgress.value = 100
   await $fetch('/api/deploy', { method: 'POST' }).catch((err) => {
     toast.add({
@@ -50,13 +49,6 @@ async function handleUpload(event: SubmitEvent) {
     timeout: 0,
   })
 }
-
-/** in megabytes */
-const fileSize = ref<number>()
-const { message: estimatedTime, mbps } = useUploadEstimate(fileSize)
-const { isSupported: isNetworkSupported } = useNetwork()
-
-const timer = ref(0)
 </script>
 
 <template>
@@ -74,20 +66,11 @@ const timer = ref(0)
       @submit.prevent="handleUpload($event as SubmitEvent)"
     >
       <UFormGroup label="Upload reports file">
-        <UInput type="file" name="file" icon="i-lucide-file-archive" required @change="fileSize = ($event?.[0]?.size) ?? undefined" />
+        <UInput type="file" name="file" icon="i-lucide-file-archive" required />
         <template #help>
-          <ClientOnly>
-            <template #fallback>
-              <span>&nbsp;</span>
-            </template>
-            <span v-if="isNetworkSupported">
-              Based off your estimated network speed of {{ mbps }} Mbps, your upload should take {{ estimatedTime }} <br>
-              time: {{ timer }}
-            </span>
-            <span v-else>
-              Upload could take several minutes
-            </span>
-          </ClientOnly>
+          <span>
+            Upload could take several minutes
+          </span>
         </template>
       </UFormGroup>
       <UButton
